bfbe3f2fcb0ff5f82c4b18ead51072866e4e65fc
Hua Do Gia Khanh <khanh.hua@axa.com.sg>
2018-10-09 11:12:20 +0800

    Added skeleton for sse-client test

---
+++ services/src/sse-client.spec.js
@@ -0,0 +1,21 @@
+import { query, registerClient, sendTo } from './sse-client';
+
+describe('sse-client', () => {
+  describe('query', () => {
+    it('should pass', () => {
+
+    });
+  });
+
+  describe('registerClient', () => {
+    it('should pass', () => {
+
+    });
+  });
+
+  describe('sendTo', () => {
+    it('should pass', () => {
+
+    });
+  });
+});
