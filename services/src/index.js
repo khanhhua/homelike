6669027a83bb6d6a837b36e1fa2b6a4577b19890
Hua Do Gia Khanh <khanh.hua@axa.com.sg>
2018-10-10 08:52:19 +0800

    WIP: Integrating with consul and fabio

--- services/src/index.js
+++ services/src/index.js
@@ -1,9 +1,10 @@
-import http from 'http';
+import { createServer } from 'http';
 
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 
 import SSE from 'sse';
+import Consul from 'consul';
 import debug from 'debug';
 
 import internalHandlers from './internal-handlers';
@@ -11,9 +12,17 @@ import { registerClient } from './sse-client';
 
 const app = new Koa();
 app.use(bodyParser());
+app.use(async (ctx, next) => {
+  if (ctx.path === '/health') {
+    ctx.body = 'OK';
+
+    return;
+  }
+  next();
+});
 internalHandlers(app);
 
-const server = http.createServer(app.callback()); // eslint-disable-line
+const server = createServer(app.callback()); // eslint-disable-line
 const sse = new SSE(server, {
   path: '/sse/*',
 });
@@ -24,6 +33,31 @@ sse.on('connection', (clientSocket, query) => {
   dbg('Connection inbound');
   registerClient(clientSocket, query);
 });
-server.listen(3838, () => {
-  dbg('TODO: Join FabioLB');
+server.listen(3838, async () => {
+  const CONSUL_HOST = process.env.CONSUL_HOST || 'localhost:8500';
+  const SERVICE_ID = 'sse-connector-1';
+  dbg(`Joining consul ${CONSUL_HOST}...`);
+
+  const consul = Consul({ promisify: true });
+  try {
+    await consul.agent.service.register({
+      name: 'sse-connector',
+      id: SERVICE_ID,
+      enableTagOverride: false,
+      tags: [
+        'urlprefix-/api/clients',
+        'urlprefix-/api/chats',
+        'urlprefix-/sse'],
+      address: '192.168.0.186',
+      port: 3838,
+      check: {
+        http: 'http://192.168.0.186:3838/health',
+        interval: '15s',
+      },
+      notes: 'Simple SSE Connector check',
+    });
+    dbg('Joined successfully');
+  } catch (e) {
+    dbg(e);
+  }
 });
