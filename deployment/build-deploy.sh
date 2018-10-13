b518066a42e9c44ddbfdf3530d0c4d4355538ad4
Hua Do Gia Khanh <khanh.hua@axa.com.sg>
2018-10-14 05:21:22 +0800

    Added build-deploy.sh

---
+++ deployment/build-deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "Building client-app package with NPM..."
+npm --prefix ../client-app run build
+
+echo "Firing up docker-compose..."
+docker-compose up -d
+
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:9999/index.html)" != "200" ]];
+do
+    printf "."
+    sleep 1;
+    echo
+done
+
+echo "Opening http://localhost:9999/index.html"
+
+open 'http://localhost:9999/index.html'
